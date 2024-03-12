1a0358fc43893d33486751e7e47d12f9f8a7da33
Gika M Pramudita <gikamegawan@aol.com>
2024-03-12 12:43:48 +0000

    Update ec2_create_snapshot.sh

--- ec2/ec2_create_snapshot.sh
+++ ec2/ec2_create_snapshot.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-while read INSTANCE; do
-  VOLUMEID=`aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE" --query "Reservations[*].Instances[*].[BlockDeviceMappings[0].Ebs.VolumeId]" --profile sandbox --output text`
-  echo "instance name=$INSTANCE, volumeId=$VOLUMEID"
-  timestamp=$(date "+%H:%M:%S %d/%m/%Y")
-  aws ec2 create-snapshot --profile sandbox --volume-id $VOLUMEID --description "Created by script at $timestamp" --tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=$INSTANCE}]"
+while IFS= read -r instance_name; do
+  volume_id=$(aws ec2 describe-instances \
+              --filters "Name=tag:Name,Values=$instance_name" \
+              --query "Reservations[*].Instances[*].BlockDeviceMappings[0].Ebs.VolumeId" \
+              --profile sandbox --output text)
+
+  if [ -n "$volume_id" ]; then
+    echo "Instance Name: $instance_name, Volume ID: $volume_id"
+    timestamp=$(date "+%H:%M:%S %d/%m/%Y")
+    description="Created by script at $timestamp"
+
+    aws ec2 create-snapshot \
+      --profile sandbox \
+      --volume-id "$volume_id" \
+      --description "$description" \
+      --tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=$instance_name}]"
+  else
+    echo "Failed to find volume for instance: $instance_name"
+  fi
 done < instance.txt
