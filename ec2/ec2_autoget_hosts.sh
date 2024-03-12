24cb9d15e434a89623851d6dfe40895d2add17cd
Gika M Pramudita <gikamegawan@aol.com>
2024-03-12 12:42:53 +0000

    Update ec2_autoget_hosts.sh

--- ec2/ec2_autoget_hosts.sh
+++ ec2/ec2_autoget_hosts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-aws ec2 describe-instances --profile root --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text > /etc/hosts
-aws ec2 describe-instances --profile development --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text >> /etc/hosts
-aws ec2 describe-instances --profile staging --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text >> /etc/hosts
-aws ec2 describe-instances --profile production --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text >> /etc/hosts
-aws ec2 describe-instances --profile monitoring --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text >> /etc/hosts
-aws ec2 describe-instances --profile ops --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' --output text >> /etc/hosts
+# Define an array of AWS profiles
+profiles=("root" "development" "staging" "production" "monitoring" "ops")
+
+# Loop through the profiles and append the instance details to /etc/hosts
+for profile in "${profiles[@]}"; do
+    aws ec2 describe-instances \
+        --profile "$profile" \
+        --query 'Reservations[].Instances[].{IP: PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' \
+        --output text >> /etc/hosts
+done
